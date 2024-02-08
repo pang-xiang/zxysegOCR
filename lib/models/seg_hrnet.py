4e82b1b2d2c71d89a24ea46df9e8ecd5813873dc
Matrix <pangxiangpx@163.com>
2024-02-08 07:32:55 +0000

    在dcn的基础上加入了aspp模块

--- lib/models/seg_hrnet.py
+++ lib/models/seg_hrnet.py
@@ -29,6 +29,64 @@ ALIGN_CORNERS = None
 
 logger = logging.getLogger(__name__)
 
+
+class ASPP(nn.Module):
+    def __init__(self, dim_in, dim_out, rate=1, bn_mom=0.1):
+        super(ASPP, self).__init__()
+        self.branch1 = nn.Sequential(
+            nn.Conv2d(dim_in, dim_out, kernel_size=(1,1), stride=(1,1), padding=0, dilation=rate, bias=True),
+            nn.BatchNorm2d(dim_out, momentum=bn_mom),
+            nn.ReLU(inplace=True),
+        )
+        self.branch2 = nn.Sequential(
+            nn.Conv2d(dim_in, dim_out, kernel_size=(3,3), stride=(1,1), padding=6 * rate, dilation=6 * rate, bias=True),
+            nn.BatchNorm2d(dim_out, momentum=bn_mom),
+            nn.ReLU(inplace=True),
+        )
+        self.branch3 = nn.Sequential(
+            nn.Conv2d(dim_in, dim_out, kernel_size=(3,3), stride=(1,1), padding=12 * rate, dilation=12 * rate, bias=True),
+            nn.BatchNorm2d(dim_out, momentum=bn_mom),
+            nn.ReLU(inplace=True),
+        )
+        self.branch4 = nn.Sequential(
+            nn.Conv2d(dim_in, dim_out, kernel_size=(3,3), stride=(1,1), padding=18 * rate, dilation=18 * rate, bias=True),
+            nn.BatchNorm2d(dim_out, momentum=bn_mom),
+            nn.ReLU(inplace=True),
+        )
+        self.branch5_conv = nn.Conv2d(dim_in, dim_out, kernel_size=(1,1), stride=(1,1), padding=0, bias=True)
+        self.branch5_bn = nn.BatchNorm2d(dim_out, momentum=bn_mom)
+        self.branch5_relu = nn.ReLU(inplace=True)
+ 
+        self.conv_cat = nn.Sequential(
+            nn.Conv2d(dim_out * 5, dim_out ,kernel_size=(1,1), stride=(1,1), padding=0, bias=True),
+            nn.BatchNorm2d(dim_out, momentum=bn_mom),
+            nn.ReLU(inplace=True),
+        )
+ 
+    def forward(self, x):
+        [b, c, row, col] = x.size()
+  
+        # 五个分支
+        conv1x1 = self.branch1(x)
+        conv3x3_1 = self.branch2(x)
+        conv3x3_2 = self.branch3(x)
+        conv3x3_3 = self.branch4(x)
+   
+        # 第五个分支，进行全局平均池化+卷积
+        global_feature = torch.mean(x, 2, True)
+        global_feature = torch.mean(global_feature, 3, True)
+        global_feature = self.branch5_conv(global_feature)
+        global_feature = self.branch5_bn(global_feature)
+        global_feature = self.branch5_relu(global_feature)
+        global_feature = F.interpolate(global_feature, (row, col), None, 'bilinear', True)
+  
+        # 五个分支的内容堆叠起来，然后1x1卷积整合特征。
+        feature_cat = torch.cat([conv1x1, conv3x3_1, conv3x3_2, conv3x3_3, global_feature], dim=1)
+        result = self.conv_cat(feature_cat)
+        return result
+
+
+
 def conv3x3(in_planes, out_planes, stride=1):
     """3x3 convolution with padding"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
@@ -315,7 +373,8 @@ class HighResolutionNet(nn.Module):
         self.stage4, pre_stage_channels = self._make_stage(
             self.stage4_cfg, num_channels, multi_scale_output=True)
         
-        last_inp_channels = np.int_(np.sum(pre_stage_channels))
+        # last_inp_channels = np.int_(np.sum(pre_stage_channels))
+        last_inp_channels = 768
 
         self.last_layer = nn.Sequential(
             nn.Conv2d(
@@ -404,7 +463,9 @@ class HighResolutionNet(nn.Module):
         self.Deconv3 = nn.Sequential(nn.ConvTranspose2d(in_channels=384, out_channels=384, kernel_size=3, stride=8, padding=1, output_padding=7), 
                                   nn.BatchNorm2d(384, momentum=BN_MOMENTUM),
                                   nn.ReLU(inplace=True))   
-                        
+
+
+        self.ASPP = ASPP(dim_in=48, dim_out=48, rate=1, bn_mom=0.1)    
 
     def _make_transition_layer(
             self, num_channels_pre_layer, num_channels_cur_layer):
@@ -542,17 +603,19 @@ class HighResolutionNet(nn.Module):
         x1 = self.Deconv1(x1) #torch.Size([1, 96, 128, 256])
         x2 = self.Deconv2(x2) #torch.Size([1, 192, 126, 254])
         x3 = self.Deconv3(x3) #torch.Size([1, 384, 122, 250])
-
-        x = torch.cat([x0, x1, x2, x3], 1)
-
+        x_dcn = torch.cat([x0, x1, x2, x3], 1)
+        aspp_out = self.ASPP(x[0])
+        x = torch.cat([x_dcn, aspp_out], 1)
+        
         x = self.last_layer(x)
+        # pdb.set_trace()
 
         return x
 
     def init_weights(self, pretrained='',):
         logger.info('=> init weights from normal distribution')
         for name, m in self.named_modules():
-            if any(part in name for part in {'DCN', 'Deconv'}):
+            if any(part in name for part in {'DCN', 'Deconv','ASPP'}):
                 # print('skipped', name)
                 continue
             if isinstance(m, nn.Conv2d):
